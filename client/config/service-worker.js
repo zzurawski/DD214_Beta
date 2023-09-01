c2a92070fbf99e573b27f3b56680381b256ad28a
zzurawski <116835382+zzurawski@users.noreply.github.com>
2023-09-01 13:30:57 -0400

    finishing touches to work with DNS

--- client/config/service-worker.js
+++ client/config/service-worker.js
@@ -1,3 +1,6 @@
 // This is intentionally left blank as a placeholder for the Webpack plugin.
 // It will be replaced with the generated service worker code during the build process.
 // Do not modify or delete this file.
+import {precacheAndRoute} from 'workbox-precaching';
+
+precacheAndRoute(self.__WB_MANIFEST);
